c4949162adfa5f1dee5ef6cdab28c1cca0f02867
Martin Devillers <martin@devillers.nl>
2020-04-29 02:28:19 -0400

    refactor: add missing TypeScript module declarations

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,9 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 declare module "react-jsx-highcharts"
+
 declare module "@mdx-js/react" {
   export const MDXProvider: (props: any) => JSX.Element
 }
+
 declare module "*.mdx" {
   const MDXComponent: (props: any) => JSX.Element
   export default MDXComponent
+}
+
+declare module "@theme-ui/color" {
+  import { Theme } from "theme-ui"
+
+  export const alpha: (c: string, n: number) => (t: Theme) => string
+}
+
+declare module "@lekoarts/gatsby-theme-minimal-blog/src/gatsby-plugin-theme-ui" {
+  import type { Theme } from "theme-ui"
+
+  const theme: Theme
+  export default theme
 }
