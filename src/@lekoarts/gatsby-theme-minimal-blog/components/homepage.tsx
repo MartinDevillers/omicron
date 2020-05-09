d16fe55371f89e2dc2dd3e766cbea4d75195ea8e
Martin Devillers <martin@devillers.nl>
2020-05-08 22:53:49 -0400

    feat: show posts and algorithms on landing page

--- src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
-import Hero from "../texts/hero.mdx"
-
-export default () => (
-  <Layout>
-    <Hero />
-  </Layout>
-)
