13c6287d7caa610f61623dae3ef52bd8bbca04a1
Martin Devillers <martin@devillers.nl>
2020-05-23 15:21:31 -0400

    feat: truncate data set definition list by default

--- src/components/data-sets-definitions.tsx
+++ src/components/data-sets-definitions.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import { Grid, Box } from "theme-ui"
-import { DataSets } from "../data-sets"
+import { Grid, Box, Button } from "theme-ui"
+import { DataSet, DataSets } from "../data-sets"
 import { sequenceness, sortedness, uniqueness } from "../util/array-stats"
 
 const formatPercentage = (percentage: number) => `${(percentage * 100).toFixed()}%`
@@ -25,33 +25,45 @@ const styles = {
   },
 }
 
-const DataSetsDefinitions = () =>
-  DataSets.all.map((x) => (
-    <Grid key={x.name} gap={[1, 2, 2]} mb={[3, 4, 4]} columns="min-content auto" sx={styles}>
-      <Box>Name</Box>
-      <Box>{x.name}</Box>
-      <Box>Description</Box>
-      <Box>{x.description}</Box>
-      <Box>Uniqueness</Box>
-      <Box>{formatPercentage(uniqueness(x.generate(1000)))}</Box>
-      <Box>Sortedness</Box>
-      <Box>{formatPercentage(sortedness(x.generate(1000)))}</Box>
-      <Box>Sequenceness</Box>
-      <Box>{formatPercentage(sequenceness(x.generate(1000)))}</Box>
-      <Box>
-        Example <code>(n=30)</code>
-      </Box>
-      <Box>
-        <code>
-          [
-          {x
-            .generate(30)
-            .map((y) => y.toFixed())
-            .join(", ")}
-          ]
-        </code>
-      </Box>
-    </Grid>
-  ))
+const renderDataSet = (dataSet: DataSet) => (
+  <Grid key={dataSet.name} gap={[1, 2, 2]} mb={[3, 4, 4]} columns="min-content auto" sx={styles}>
+    <Box>Name</Box>
+    <Box>{dataSet.name}</Box>
+    <Box>Description</Box>
+    <Box>{dataSet.description}</Box>
+    <Box>Uniqueness</Box>
+    <Box>{formatPercentage(uniqueness(dataSet.generate(1000)))}</Box>
+    <Box>Sortedness</Box>
+    <Box>{formatPercentage(sortedness(dataSet.generate(1000)))}</Box>
+    <Box>Sequenceness</Box>
+    <Box>{formatPercentage(sequenceness(dataSet.generate(1000)))}</Box>
+    <Box>
+      Example <code>(n=30)</code>
+    </Box>
+    <Box>
+      <code>
+        [
+        {dataSet
+          .generate(30)
+          .map((y) => y.toFixed())
+          .join(", ")}
+        ]
+      </code>
+    </Box>
+  </Grid>
+)
+
+const DataSetsDefinitions = () => {
+  const reducer = (state: DataSet[]) => state.concat(DataSets.all.slice(state.length, state.length + 5))
+  const [dataSets, showMoreDataSets] = React.useReducer(reducer, DataSets.all.slice(0, 5))
+  const buttonVariant = dataSets.length < DataSets.all.length ? "outline" : "disabled"
+
+  return (
+    <>
+      {dataSets.map(renderDataSet)}
+      <Button variant={buttonVariant} onClick={showMoreDataSets}>Show more data sets</Button>
+    </>
+  )
+}
 
 export default DataSetsDefinitions
